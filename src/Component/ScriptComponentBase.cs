d86c1dfad656ca916d5b08f83eb703f72d1f88a5
skclusive <skclusive@outlook.com>
2019-12-25 23:46:02 +0530

    ScriptComponentBase added

---
+++ src/Component/ScriptComponentBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Skclusive.Core.Component
+{
+    public abstract class ScriptComponentBase : StaticComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "script");
+            builder.AddMarkupContent(1, GetScript());
+            builder.CloseElement();
+        }
+
+        protected abstract string GetScript();
+    }
+}
