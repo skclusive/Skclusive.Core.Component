e6c60da1d8e3993412fda375c79ef7bcacfb3eec
skclusive <skclusive@outlook.com>
2020-08-30 19:57:49 +0530

    service descriptor api added

---
+++ src/Extension/ServiceExtentions.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Skclusive.Core.Component
+{
+    public static class ServiceExtentions
+    {
+        public static void TryAddTransientEnumerable<TService, TImplementation>(this IServiceCollection collection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            var serviceDescriptor = ServiceDescriptor.Transient<TService, TImplementation>();
+
+            collection.TryAddEnumerable(serviceDescriptor);
+        }
+
+        public static void TryAddScopedEnumerable<TService, TImplementation>(this IServiceCollection collection)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            var serviceDescriptor = ServiceDescriptor.Scoped<TService, TImplementation>();
+
+            collection.TryAddEnumerable(serviceDescriptor);
+        }
+
+        public static void TryAddStyleTypeProvider<TStyleTypeProvider>(this IServiceCollection collection) where TStyleTypeProvider : class, IStyleTypeProvider
+        {
+            collection.TryAddScopedEnumerable<IStyleTypeProvider, TStyleTypeProvider>();
+        }
+
+        public static void TryAddScriptTypeProvider<TScriptTypeProvider>(this IServiceCollection collection) where TScriptTypeProvider : class, IScriptTypeProvider
+        {
+            collection.TryAddScopedEnumerable<IScriptTypeProvider, TScriptTypeProvider>();
+        }
+    }
+}
