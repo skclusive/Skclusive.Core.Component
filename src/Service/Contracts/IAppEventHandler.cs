877c342cc2d365fe84c55b670d85bc8b1cf39b25
skclusive <skclusive@outlook.com>
2020-09-30 22:11:27 +0530

    app event handler added

---
+++ src/Service/Contracts/IAppEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Skclusive.Core.Component
+{
+    public interface IAppEventHandler : IAsyncDisposable
+    {
+        Task OnInitializedAsync();
+
+        Task OnMountedAsync();
+    }
+}
