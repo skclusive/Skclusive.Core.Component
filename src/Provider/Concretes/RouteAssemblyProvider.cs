50f221d70149bc2d9f11044fb0e3cadb73941145
skclusive <skclusive@outlook.com>
2020-09-30 12:23:03 +0530

    Route assembly provider

---
+++ src/Provider/Concretes/RouteAssemblyProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Skclusive.Core.Component
+{
+    public class RouteAssemblyProvider : IRouteAssemblyProvider
+    {
+        public IEnumerable<Assembly> Assemblies { get; }
+
+        public RouteAssemblyProvider(params Assembly [] assemblies)
+        {
+            Assemblies = assemblies;
+        }
+    }
+
+    public class RouteAssemblyProvider<T> : RouteAssemblyProvider
+    {
+        public RouteAssemblyProvider() : base(typeof(T).Assembly)
+        {
+        }
+    }
+}
---
+++ src/Provider/Contracts/IRouteAssemblyProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Skclusive.Core.Component
+{
+    public interface IRouteAssemblyProvider
+    {
+        IEnumerable<Assembly> Assemblies { get; }
+    }
+}
